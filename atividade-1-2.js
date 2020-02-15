22df3e28301d9512d4a36dc4949ebd2b8380851e
Evandro Carlos <evandrorocha20@gmail.com>
2020-02-14 21:56:02 -0300

    atividade 1-2 feita

--- atividade-1-2.js
+++ atividade-1-2.js
@@ -1,6 +1,9 @@
 var user = require('readline-sync');
 var numero = user.question("Insira um numero para saber se e par ou impar: ");
-if{numero % 2 == 0
+if(numero % 2 == 0){
+
     console.log(" Esse numero e par ");
+}else{
+    console.log(" Esse numero e impar ")
 }
 
