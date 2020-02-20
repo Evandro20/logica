3b55d9e6622df7b8782a699202658de7d6ef4d2b
Evandro Carlos <evandrorocha20@gmail.com>
2020-02-20 10:06:23 -0300

    atividades

--- atividade-1-7.js
+++ atividade-1-7.js
@@ -5,4 +5,10 @@ var altura = user.question( " Quantos centimetros tem um dos lados? ")
 var base = altura
 if( lados == 3){
     console.log(" Seu Poligono regular e um Triangulo e area e :" + (base*altura)/2 )
+}else if( lados == 4){
+    console.log( " Seu poligono regular e um Quadrado e a area e: " +base*altura)
+}else if( lados == 5){
+    console.log( " Seu poligono regular e um Pentagono! ")
+}else{
+    console.log( " Digite um numero valido. ")
 }
