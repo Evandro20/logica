a99a262c6811a77bacf95790ca27f10fc4790031
Evandro Carlos <evandrorocha20@gmail.com>
2020-02-28 13:58:36 -0300

    continuaçaõ dos exercicios

--- atividade-2-8.js
+++ atividade-2-8.js
@@ -1,11 +1,7 @@
 var user = require('readline-sync')
 var palavra = user.question( " Escreva alguma palavra: " )
-palavra = palavra.charAt(0)
-palavra = palavra.length
- if( palavra == 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z'){
-     console.log( " Sua primeira letra e minuscula ")
- }else if( palavra == 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z'){
-     console.log( " Sua primeira letra e maiuscula. ")
- }else{
-     console.log( " Favor escrever uma palavra. ")
- }
+if( palavra[0].toUpperCase(0) === palavra[0]){
+    console.log ( " Maiuscula ")
+}else{
+    console.log( " Minuscula")
+}
--- atividade-3-7.js
+++ atividade-3-7.js
@@ -1,2 +1,32 @@
 var user = require('readline-sync')
-var programa = user.questionInt
+var programa = user.question( " Insira A: para fechar o programa e B para executar uma soma ")
+var num1
+var num2
+var soma = "R"
+
+while( programa !== "A"){
+    if( programa == "A"){
+        process.exit
+    } else if ( programa === "B"){
+        while( soma === "R"){
+            num1 = user.questionInt( " Insira o Primeiro numero : ")
+            num2 = user.questionInt( " Insira o Segundo numero : ")
+            console.log ( " A soma e : " + (num1 + num2))
+            soma = user.question ( " Insira R para repetir a soma e S para sair ")
+            if ( soma == "S"){
+                console.log( " Programa encerrado! ")
+                process.exit()
+            } while ( soma !== "R"){
+                console.log ( " Valor invalido ")
+                soma = user.question( " Insira R ou S ")
+                if ( soma ==="S"){
+                    console.log ( " Programa encerrado!")
+                    process.exit()
+                }
+            }
+        }
+    }else{
+        console.log ( " Valor Invalido ")
+        programa = user.question ( " Insira A ou B ")
+    }
+}
