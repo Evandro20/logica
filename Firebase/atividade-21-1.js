622b981b893722fba990c7de7414df5c9bc7e94d
Evandro Carlos <evandrorocha20@gmail.com>
2020-04-06 16:50:57 -0300

    atividade 21

--- Firebase/atividade-21-1.js
+++ Firebase/atividade-21-1.js
@@ -1,79 +1,113 @@
-
 var admin = require("firebase-admin");
 var serviceAccount = require("./atividadelista21.json");
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://atividade-21-345ed.firebaseio.com"
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://atividade-21-345ed.firebaseio.com"
 });
 var carros = "Carros"
 var db = admin.database().ref(carros)
 var user = require('readline-sync')
 
-var pergunta = user.questionInt('\nInsira 1: Para cadastrar Carro.\nInsira 2: Para mostrar carros cadastrados.\nInsira 3: Para pesquisar um valor e saber quais carros estao acima desse valor.\nInsira 4: Para pesquisar um valor e saber quais carros estao abaixo desse valor.\nInsira 5: Para pesquisar um valor e saber quais carros estao com o valor informado.\nInsira 6: Para sair.\n')
+async function main() {
+    var pergunta = user.questionInt('\nInsira 1: Para cadastrar Carro.\nInsira 2: Para mostrar carros cadastrados.\nInsira 3: Para pesquisar um valor e saber quais carros estao acima desse valor.\nInsira 4: Para pesquisar um valor e saber quais carros estao abaixo desse valor.\nInsira 5: Para pesquisar um valor e saber quais carros estao com o valor informado.\nInsira 6: Para sair.\n')
+    while (pergunta !== 6) {
+        console.clear()
+        if (pergunta === 1) {
+            await cadastrarCarro().then(p => {
+                console.log(p)
+            })
+        }
+        if (pergunta === 2) {
+            console.log('carregando...')
+            await exibirCarro().then(p => {
+                console.clear()
+                console.table(p)
+            })
+
+        }
+        if (pergunta === 3) {
+            await mostrarMaior().then(p => {
+                console.clear()
+                console.table(p)
+            })
+        }
+        if (pergunta === 4) {
+            await mostrarMenor().then(p => {
+                console.clear()
+                console.table(p)
+            })
+        }
+        if (pergunta === 5) {
+            await mostrarIgual().then(p => {
+                console.clear()
+                console.table(p)
+            })
+        }
+        user.question('Pressione enter para continuar...')
+        console.clear()
+        pergunta = user.questionInt('\nInsira 1: Para cadastrar Carro.\nInsira 2: Para mostrar carros cadastrados.\nInsira 3: Para pesquisar um valor e saber quais carros estao acima desse valor.\nInsira 4: Para pesquisar um valor e saber quais carros estao abaixo desse valor.\nInsira 5: Para pesquisar um valor e saber quais carros estao com o valor informado.\nInsira 6: Para sair.\n')
+    }
+    process.exit()
+}
 
 function cadastrarCarro() {
-  var nome = user.question('\nInsira o nome do Carro: ')
-  var valor = user.questionInt('\nInsira o Valor do Carro:')
-  db.push({
-    nome: nome,
-    valor: valor
-  })
-  console.log('\nCarro Cadastrado!')
+    return new Promise(async (resolve, reject) => {
+        var nome = user.question('\nInsira o nome do Carro: ')
+        var valor = user.questionInt('\nInsira o Valor do Carro:')
+        db.push({
+            nome: nome,
+            valor: valor
+        })
+        resolve('\nCarro Cadastrado!')
+            , err => {
+                reject(err)
+            }
+    })
 }
 
 function exibirCarro() {
-  db.on('value', snapshot => {
-    console.log(snapshot.val())
-    console.log('\nEsta e a lista de Carro! ')
-  })
-
+    return new Promise(async (resolve, reject) => {
+        db.on('value', snapshot => {
+            resolve(snapshot.val())
+        }, err => {
+            reject(err)
+        })
+    })
 }
+
 function mostrarMaior() {
-  var maior = user.questionInt('\nInsira o valor: ')
-  db.orderByChild('valor').startAt(maior)
-    .on("child_added", snapshot => {
-      console.log(snapshot.val())
-      console.log('\n Aqui esta a lista de carros!')
+    return new Promise(async (resolve, reject) => {
+        var maior = user.questionInt('\nInsira o valor: ')
+        db.orderByChild('valor').startAt(maior)
+            .on("value", snapshot => {
+                resolve(snapshot.val())
+            }), err => {
+                reject(err)
+            }
     })
 }
 function mostrarMenor() {
-  var menor = user.questionInt('\nInsira o Valor: ')
-  db.orderByChild('valor').endAt(menor).on('child_added', snapshot => {
-    console.log(snapshot.val())
-    console.log('\n A lista de carros esta aqui! ')
-  })
+    return new Promise(async (resolve, reject) => {
+        var menor = user.questionInt('\nInsira o Valor: ')
+        db.orderByChild('valor').endAt(menor).on('value', snapshot => {
+            resolve(snapshot.val())
+        }), err => {
+            reject(err)
+        }
+    })
 }
 
 function mostrarIgual() {
-  var igual = user.questionInt('\nInsira um valor: ')
-  db.orderByChild('valor').equalTo(igual)
-    .on("child_added", snapshot => {
-      console.log(snapshot.val())
-      console.log('\n A lista de carros esta aqui! ')
+    return new Promise(async (resolve, reject) => {
+        var igual = user.questionInt('\nInsira um valor: ')
+        db.orderByChild('valor').equalTo(igual)
+            .on("value", snapshot => {
+                resolve(snapshot.val())
+            }), err => {
+                reject(err)
+            }
     })
-
-    
 }
 
-while (pergunta !== 6) {
-  if (pergunta == 1) {
-    cadastrarCarro()
-  }
-  else if (pergunta == 2) {
-    exibirCarro()
-  }
-  else if (pergunta == 3) {
-    mostrarMaior()
-  }
-  else if (pergunta == 4) {
-    mostrarMenor()
-  }
-  else if (pergunta == 5) {
-    mostrarIgual()
-  }
-  else {
-    console.log('Numero Invalido!!!')
-  }
-  var pergunta = user.questionInt('Insira 1: Para cadastrar Carro.\nInsira 2: Para mostrar carros cadastrados.\nInsira 3: Para pesquisar um valor e saber quais carros estao acima desse valor.\nInsira 4: Para pesquisar um valor e saber quais carros estao abaixo desse valor.\nInsira 5: Para pesquisar um valor e saber quais carros estao com o valor informado.\nInsira 6: Para sair.\n')
 
-}
+main()
